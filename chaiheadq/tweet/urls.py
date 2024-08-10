0be585ddc44e6a9bf68b57645499c7153f88ff7e
Jatin <chaurasiyajatin68@gmail.com>
2024-08-10 19:19:14 +0530

    first commit

---
+++ chaiheadq/tweet/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.tweet_list, name = 'tweet_list'),
+    path('create/', views.tweet_create, name = 'tweet_create'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name = 'tweet_delete'),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name = 'tweet_edit'),
+    path('register/', views.register, name ='register'),
+
+
+]
